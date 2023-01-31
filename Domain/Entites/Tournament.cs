fe59efd013f7d2e4f238304c9e85cd415d6ef62e
laghribi98 <laghribi98@gmail.com>
2023-02-01 00:44:11 +0100

    SYNC

--- Domain/Entites/Tournament.cs
+++ Domain/Entites/Tournament.cs
@@ -3,8 +3,13 @@ namespace Domain.Entites
 {
 	public class Tournament
 	{
+		public Guid Id { get; set; }
+		public String Name { get; set; }
+		public DateTime CreatedAt { get; set; }
+
 		public Tournament()
 		{
+
 		}
 	}
 }
