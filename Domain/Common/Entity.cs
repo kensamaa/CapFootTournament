c5ab4befbe10773775c499667301b544fc76f5cb
laghribi98 <laghribi98@gmail.com>
2023-02-02 18:28:53 +0100

    Tournament second implementation

--- Domain/Common/Entity.cs
+++ Domain/Common/Entity.cs
@@ -4,8 +4,6 @@ namespace Domain.Common;
 public abstract class Entity
 {
 	public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public String CountryOrganisation { get; set; } = string.Empty;
     public DateTime? DateCreated { get; set; }
 	public DateTime? DateModified { get; set; }
 }
