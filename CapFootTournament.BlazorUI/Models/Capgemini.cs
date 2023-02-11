cb3634dde0a56548c95967c4f49122b3f8e8419c
kensamaa <amineab203@gmail.com>
2023-02-11 22:51:26 +0100

    adding the capgemini table

--- CapFootTournament.BlazorUI/Models/Capgemini.cs
+++ CapFootTournament.BlazorUI/Models/Capgemini.cs
@@ -2,6 +2,7 @@
 
 public class Capgemini
 {
+	public string Id { get; set; }
 	public string Name { get; set; } = string.Empty;
 	public string Country { get; set; } = string.Empty;
 	public string City { get; set; } = string.Empty;
