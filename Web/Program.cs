f39a9af3f8bee4b39937d6b1a9c6f8fbb5876b1d
kensamaa <amineab203@gmail.com>
2023-02-11 17:24:43 +0100

    add map controllers ( api works !!)

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-using Application;
+﻿using Application;
 using Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,31 +33,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
