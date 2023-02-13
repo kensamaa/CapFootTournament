1abc18110b0579ccef719024749fc3d13bdb9440
laghribi98 <laghribi98@gmail.com>
2023-02-13 23:19:45 +0100

    Add OneToMany Tournament And Groups

--- Domain/Entites/Tournament.cs
+++ Domain/Entites/Tournament.cs
@@ -7,13 +7,15 @@ public class Tournament:Entity
 {
     public Tournament() {
         this.capgeminiTournament =  new List<CapgeminiTournament>();
+        this.ListeGroupes = new List<Groupe>();
     }
     public String Name { get; set; } = string.Empty;
     public String CountryOrganisation { get; set; } = string.Empty;
     public Boolean isStarted { get; set; }= false;
-    public Boolean isSignUpCompleted { get; set; }
-    public Boolean isFinished { get; set; }
-    
+    public Boolean isSignUpCompleted { get; set; }= false;
+    public Boolean isFinished { get; set; } = false;
+
     public virtual List<CapgeminiTournament> capgeminiTournament { get; set; }
+    public virtual List<Groupe> ListeGroupes { get; set; }
 }
 
